efbbde3aa7536f002675543c055ef3a829429e05
杨伟杰 <justlikesuolong@outlook.com>
2020-03-08 13:55:08 +0800

    上级权限修复

--- app/Handle/Admin/Permission/Add.php
+++ app/Handle/Admin/Permission/Add.php
@@ -17,24 +17,26 @@ class Add implements RequestHandlerInterface
         $post = $request->getParsedBody();
 
         Validator::validate($post, [
-            'menu'   => 'must&&int&&intIn:' . implode(',', [STATUS['Yes'], STATUS['No']]),
-            'path'   => 'must&&string&&stringMax:100',
-            'name'   => 'must&&string&&safe&&stringBetween:4,40',
-            'memo'   => 'must&&string&&stringMax:200',
-            'status' => 'must&&int&&intIn:' . implode(',', STATUS),
-            'icon'   => 'must&&string&&safe&&stringMax:20'
+            'parent_code' => 'string&&safe&&stringMax:40',
+            'menu'        => 'must&&int&&intIn:' . implode(',', [STATUS['Yes'], STATUS['No']]),
+            'path'        => 'must&&string&&stringMax:100',
+            'name'        => 'must&&string&&safe&&stringBetween:4,40',
+            'memo'        => 'must&&string&&stringMax:200',
+            'status'      => 'must&&int&&intIn:' . implode(',', STATUS),
+            'icon'        => 'must&&string&&safe&&stringMax:20'
         ]);
 
-        $post['code']            = uniqid(time());
-        $adminPermission         = new AdminPermission;
-        $adminPermission->path   = $post['path'];
-        $adminPermission->icon   = $post['icon'];
-        $adminPermission->code   = $post['code'];
-        $adminPermission->name   = $post['name'];
-        $adminPermission->memo   = $post['memo'];
-        $adminPermission->menu   = $post['menu'];
-        $adminPermission->status = $post['status'];
-        $save                    = $adminPermission->save();
+        $post['code']                 = uniqid(time());
+        $adminPermission              = new AdminPermission;
+        $adminPermission->path        = $post['path'];
+        $adminPermission->icon        = $post['icon'];
+        $adminPermission->code        = $post['code'];
+        $adminPermission->name        = $post['name'];
+        $adminPermission->memo        = $post['memo'];
+        $adminPermission->menu        = $post['menu'];
+        $adminPermission->status      = $post['status'];
+        $adminPermission->parent_code = $post['parent_code'] ?? '';
+        $save                         = $adminPermission->save();
 
         if ($save !== true) {
             throw new ExceptionBase('添加管理员权限失败');
--- app/Handle/Admin/Permission/Edit.php
+++ app/Handle/Admin/Permission/Edit.php
@@ -17,24 +17,26 @@ class Edit implements RequestHandlerInterface
         $post = $request->getParsedBody();
 
         Validator::validate($post, [
-            'code'   => 'must&&string&&safe&&stringMax:40',
-            'menu'   => 'must&&int&&intIn:' . implode(',', [STATUS['YES'], STATUS['NO']]),
-            'path'   => 'must&&string&&stringMax:100',
-            'name'   => 'must&&string&&safe&&stringBetween:4,40',
-            'memo'   => 'must&&string&&stringMax:200',
-            'status' => 'must&&int&&intIn:' . implode(',', STATUS),
-            'icon'   => 'must&&string&&safe&&stringMax:20'
+            'parent_code' => 'string&&safe&&stringMax:40',
+            'code'        => 'must&&string&&safe&&stringMax:40',
+            'menu'        => 'must&&int&&intIn:' . implode(',', [STATUS['YES'], STATUS['NO']]),
+            'path'        => 'must&&string&&stringMax:100',
+            'name'        => 'must&&string&&safe&&stringBetween:4,40',
+            'memo'        => 'must&&string&&stringMax:200',
+            'status'      => 'must&&int&&intIn:' . implode(',', STATUS),
+            'icon'        => 'must&&string&&safe&&stringMax:20'
         ]);
 
         /** @var AdminPermission */
-        $adminPermission         = AdminPermission::query()->findOrFail($post['code']);
-        $adminPermission->path   = $post['path'];
-        $adminPermission->icon   = $post['icon'];
-        $adminPermission->code   = $post['code'];
-        $adminPermission->name   = $post['name'];
-        $adminPermission->memo   = $post['memo'];
-        $adminPermission->status = $post['status'];
-        $save                    = $adminPermission->save();
+        $adminPermission              = AdminPermission::query()->findOrFail($post['code']);
+        $adminPermission->path        = $post['path'];
+        $adminPermission->icon        = $post['icon'];
+        $adminPermission->code        = $post['code'];
+        $adminPermission->name        = $post['name'];
+        $adminPermission->memo        = $post['memo'];
+        $adminPermission->status      = $post['status'];
+        $adminPermission->parent_code = $post['parent_code'];
+        $save                         = $adminPermission->save();
 
         if ($save !== true) {
             throw new ExceptionBase('修改管理员权限失败');
