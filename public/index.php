30be46488c953e6a153b5f1bc8becd15b7a671f8
杨伟杰 <justlikesuolong@outlook.com>
2020-03-17 12:20:53 +0800

    入口异常处理变更

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 
 use App\Exception\Base\ExceptionProcess;
 use App\Process\Options;
-use App\Process\ParseBody;
-use App\Process\WithConf;
 use GuzzleHttp\Psr7\ServerRequest;
 use Suolong\Psr15\Handle;
+use App\Process\WithConf;
+use App\Process\ParseBody;
 use Suolong\Psr15Router\RouteNotFound;
 use Suolong\Validator\ValidateFail;
 
@@ -23,21 +23,10 @@ $handle = new Handle([
     $router
 ]);
 
-try {
-    $request = ServerRequest::fromGlobals();
-    $result = $handle->handle($request);
-    $code = $result->getStatusCode();
-    $content = $result->getBody()->getContents();
-    $headers = $result->getHeaders();
-
-    foreach ($headers as $header => $values) {
-        header(sprintf("%s: %s", $header, implode(',', $values)));
-    }
-
-    http_response_code($code);
-    exit($content);
-} catch (Exception $e) {
-    if (CONF['app']['debug'] === true) {
+function clearUpException(Exception $e)
+{
+    if (CONF['app']['debug'] === true)
+    {
         http_response_code(500);
         throw $e;
     }
@@ -45,3 +34,41 @@ try {
     http_response_code(500);
     exit('服务器请求异常');
 }
+
+function clearUpError($errno, $errstr, $errfile, $errline)
+{
+    if (CONF['app']['debug'] === true)
+    {
+        $template = <<<EOF
+        出错文件: %s
+        对应行号: %s
+        错误编号: %s
+        错误信息: %s
+        EOF;
+        echo sprintf(
+            $template,
+            $errfile,
+            $errline,
+            $errno,
+            $errstr
+        );
+        http_response_code(500);
+    }
+}
+
+set_error_handler('clearUpError');
+set_exception_handler('clearUpException');
+
+$request = ServerRequest::fromGlobals();
+$result  = $handle->handle($request);
+$code    = $result->getStatusCode();
+$content = $result->getBody()->getContents();
+$headers = $result->getHeaders();
+
+foreach($headers as $header => $values)
+{
+    header(sprintf("%s: %s", $header, implode(',', $values)));
+}
+
+http_response_code($code);
+exit($content);
